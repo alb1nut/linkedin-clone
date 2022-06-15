605feb40f8ea9cea07731418c43fdf2162c121c2
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-15 14:23:38 +0000

    Feature: finished the Feed__top  aspect of Ui

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../styles/Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import InputOption from './InputOption';
 
 
 const Feed = () => {
@@ -15,8 +20,14 @@ const Feed = () => {
             <button type='submit'>Send</button>
           </form>
        </div>
-
+      <div className="feed__inputOptions">
+        <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9' />
+        <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E' />
+        <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD' />
+        <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E' />
       </div>
+      </div>
+      
     </div>
   )
 }
