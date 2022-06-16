c281f7f4e185c4173a0c91fb33efbd7330f3ba18
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-16 18:38:50 +0000

    Feat: finished building login Ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,28 @@ import React from 'react'
 import'../styles/Login.css'
 
 const Login = () => {
+
+const register =()=>{
+
+}
+
+const loginToApp =()=>{}
+
   return (
     <div className='login'>
-      <h1>You are not loggedin</h1>
+      <img  src='' alt='linkedin logo'/>
+
+      <form>
+        <input type="text" placeholder='Full name (required if registering)' />
+        <input type="text" placeholder='Profile pic URl (optional)' />
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <button type='submit' onClick={loginToApp}>Sign In</button>
+      </form>
+      <p>Not a member? {" "}
+
+        <span className='login__register' onClick={register}>Register Now</span>
+      </p>
     </div>
   )
 }
