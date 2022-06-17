e9345777d331538005c06ad1ad542d095e25fc4f
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-17 09:41:46 +0000

    Feat: working on redux and firebase latest versions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,90 @@
-import React from 'react'
-import'../styles/Login.css'
+import React, { useState } from "react";
+import "../styles/Login.css";
+import { auth } from "../firebase/firebase";
+import { login } from "../features/userSlice";
+import { useDispatch } from "react-redux/es/exports";
 
 const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
 
-const register =()=>{
+  const register = () => {
+    if (!name) {
+      return alert("Please Enter your full name!");
+    }
 
-}
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(login)({
+              email: userAuth.user.email,
+              uid: userAuth.user.uid,
+              displayName: name,
+              photoUrl: profilePic,
+            });
+          });
+      })
+      .catch((error) => alert(error));
+  };
 
-const loginToApp =()=>{}
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div className='login'>
-      <img  src='' alt='linkedin logo'/>
+    <div className="login">
+      <img
+        src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo.png"
+        alt="linkedin logo"
+      />
 
       <form>
-        <input type="text" placeholder='Full name (required if registering)' />
-        <input type="text" placeholder='Profile pic URl (optional)' />
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Password' />
-        <button type='submit' onClick={loginToApp}>Sign In</button>
-      </form>
-      <p>Not a member? {" "}
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full name (required if registering)"
+        />
 
-        <span className='login__register' onClick={register}>Register Now</span>
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type="text"
+          placeholder="Profile pic URl (optional)"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Login ;
+export default Login;
